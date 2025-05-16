9da82c47d98bb1f8afb5b34c49909e45e7321c29
TanvirHridoy <anxioushridoy4@gmail.com>
2025-05-16 18:13:35 +0600

    cleanup

--- UserOnboarding.Infrastructure/AppDbContext.cs
+++ UserOnboarding.Infrastructure/AppDbContext.cs
@@ -10,19 +10,7 @@ public class AppDbContext : DbContext
     public DbSet<User> Users => Set<User>();
     public DbSet<OTP> OTPs => Set<OTP>();
 
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //    modelBuilder.Entity<User>()
-    //        .HasIndex(u => u.ICNumber)
-    //        .IsUnique();
 
-    //    modelBuilder.Entity<OTP>()
-    //        .HasOne(o => o.User)
-    //        .WithMany()
-    //        .HasForeignKey(o => o.UserId);
-
-    //    base.OnModelCreating(modelBuilder);
-    //}
 }
 
 
